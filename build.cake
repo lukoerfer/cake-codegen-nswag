06e18b8298029f166c5f451bc20eb8ae6c8ec8a9
Lukas Körfer <lu.koerfer@googlemail.com>
2019-06-14 01:07:09 +0200

    Minor refactoring

--- build.cake
+++ build.cake
@@ -1,9 +1,3 @@
-#addin nuget:?package=NSwag.Core&loaddependencies=true
-#addin nuget:?package=NSwag.CodeGeneration.CSharp&loaddependencies=true
-#addin nuget:?package=NSwag.CodeGeneration.TypeScript&loaddependencies=true
-#addin nuget:?package=NSwag.Core.Yaml&loaddependencies=true
-#r "src\Cake.CodeGen.NSwag\bin\Debug\netstandard2.0\Cake.CodeGen.NSwag.dll"
-
 var target = Argument("target", "Default");
 
 Task("Test")
--- src/Cake.CodeGen.NSwag/NSwagSources.cs
+++ src/Cake.CodeGen.NSwag/NSwagSources.cs
@@ -17,27 +17,27 @@ namespace Cake.CodeGen.NSwag
             Context = context;
         }
 
-        public NSwagRunner FromJsonSpec(FilePath specification)
+        public NSwagRunner FromJsonSpecification(FilePath specification)
         {
             string path = Context.MakeAbsolute(specification).FullPath;
             OpenApiDocument document = OpenApiDocument.FromFileAsync(path).Result;
             return new NSwagRunner(Context, document);
         }
 
-        public NSwagRunner FromJsonSpec(Uri specification)
+        public NSwagRunner FromJsonSpecification(Uri specification)
         {
             OpenApiDocument document = OpenApiDocument.FromUrlAsync(specification.ToString()).Result;
             return new NSwagRunner(Context, document);
         }
 
-        public NSwagRunner FromYamlSpec(FilePath specification)
+        public NSwagRunner FromYamlSpecification(FilePath specification)
         {
             string path = Context.MakeAbsolute(specification).FullPath;
             OpenApiDocument document = OpenApiYamlDocument.FromFileAsync(path).Result;
             return new NSwagRunner(Context, document);
         }
 
-        public NSwagRunner FromYamlSpec(Uri specification)
+        public NSwagRunner FromYamlSpecification(Uri specification)
         {
             OpenApiDocument document = OpenApiYamlDocument.FromUrlAsync(specification.ToString()).Result;
             return new NSwagRunner(Context, document);
